0411f5b33213092d431241ac6ded3afd0bf5d2c6
Alex Talbott <alex.e.talbott@gmail.com>
2018-12-18 11:30:17 -0600

    build out second tab for state-level EDA

--- server.R
+++ server.R
@@ -3,15 +3,26 @@
 shinyServer(function(input, output) {
   output$deathbar <- renderPlot({
     first <- overdoses_by_year %>% group_by(State) %>% filter(year == input$radio_year, Deaths > input$opioid_deaths) %>% select(State,Population,Deaths)
-    ggplot(first) + aes(x=State, y=Deaths, fill=Population) + geom_bar(stat="identity") + coord_flip() + scale_fill_gradient(low = "blue", high = "red", name = "Population", labels = comma)
+    ggplot(first) + aes(x=State, y=Deaths, fill=Population) + geom_bar(stat="identity") + coord_flip() + scale_fill_gradient(low = "blue", high = "red", name = "Population", labels = comma) + labs(title="Total Overdose-Related Deaths")
     
   })
   output$deathcapbar <- renderPlot({
     first <- overdoses_by_year %>% group_by(State) %>% filter(year == input$radio_year, Deaths > input$opioid_deaths) %>% select(State,Population,DeathsPerCapita100k)
-    ggplot(first) + aes(x=State, y=DeathsPerCapita100k, fill=Population) + geom_bar(stat="identity") + coord_flip() + scale_fill_gradient(low = "blue", high = "red", name = "Population", labels = comma) + ylim(0,50)
+    ggplot(first) + aes(x=State, y=DeathsPerCapita100k, fill=Population) + geom_bar(stat="identity") + coord_flip() + scale_fill_gradient(low = "blue", high = "red", name = "Population", labels = comma) + ylim(0,50) + labs(y="Deaths per 100k",title="Overdose-Related Deaths per 100,000 People")
     
   })
   
+  output$singleState <- renderPlot({
+    first <- overdoses_by_year %>% group_by(State) %>% filter(year == input$radio_year, State==input$select) %>% select(State,opioidPrescriptions,OpioidPrescriptionsPerPerson)
+    ggplot(first) + aes(x=State, y=opioidPrescriptions) + geom_bar(stat="identity") + ylim(0, 200000) + labs(y="Opioid Prescriptions", title="Total Opioid Prescriptions")
+   
+  })
+  
+  output$singleStatePP <- renderPlot({
+    first <- overdoses_by_year %>% group_by(State) %>% filter(year == input$radio_year, State==input$select) %>% select(State,opioidPrescriptions,OpioidPrescriptionsPerPerson)
+    ggplot(first) + aes(x=State, y=OpioidPrescriptionsPerPerson*100) + geom_bar(stat="identity") + ylim(0, 350) + labs(y="Opioid Prescriptions per 100 People", title="Opioid Prescriptions Per 100 People")
+    
+  })  
   
   
 })
--- ui.R
+++ ui.R
@@ -1,25 +1,40 @@
 shinyUI(
   dashboardPage(
-    dashboardHeader(title = "ReliaShield Dashboard"),
+    dashboardHeader(title = "U.S. Opioid Epidemic Dashboard"),
     dashboardSidebar(
-     sliderInput(inputId = "opioid_deaths",
-                  label = "Opioid Deaths Lower Limit",
-                  min = 0,
-                  max = 4500,
-                  value = 0),
      radioButtons("radio_year", label = h3("Choose Year"),
                   choices = list("2014" = 2014, "2016" = 2016), 
                   selected = 2014)
     ),
   dashboardBody(
-     fluidRow(
-        box(
-          plotOutput("deathbar")
-        ),
-        box(
-          plotOutput("deathcapbar")
-        )
-     )
+    fluidPage(
+      tabsetPanel(
+      tabPanel("All States - Deaths",
+       sidebarLayout(position="right",
+               sidebarPanel(sliderInput(inputId = "opioid_deaths",
+                                        label = "Opioid Deaths Lower Limit",
+                                        min = 0,
+                                        max = 4500,
+                                        value = 0)),
+       fluidRow(
+          box(
+            plotOutput("deathbar")
+          ),
+          box(
+            plotOutput("deathcapbar")
+          )
+       )
+      )
+      ),
+      tabPanel("Single State - Opioid Prescriptions",
+       selectInput("select", label = h3("Select State"), 
+                   choices = list("Alabama" = "Alabama", "Alaska" = "Alaska", "Arizona" = "Arizona", "Arkansas" = "Arkansas","California"="California","Colorado"="Colorado","Connecticut"="Connecticut","Delaware"="Delaware","Florida"="Florida","Georgia"="Georgia","Hawaii"="Hawaii","Idaho"="Idaho","Illinois"="Illinois","Indiana"="Indiana","Iowa"="Iowa","Kansas"="Kansas","Kentucky"="Kentucky","Louisiana"="Louisiana","Maine"="Maine","Maryland"="Maryland","Massachusetts"="Massachusetts","Michigan"="Michigan","Minnesota"="Minnesota","Mississippi"="Mississippi","Missouri"="Missouri","Montana"="Montana","Nebraska"="Nebraska","Nevada"="Nevada","New Hampshire"="New Hampshire","New Jersey"="New Jersey","New Mexico"="New Mexico","New York"="New York","North Carolina"="North Carolina","North Dakota"="North Dakota","Ohio"="Ohio","Oklahoma"="Oklahoma","Oregon"="Oregon","Pennsylvania"="Pennsylvania","Rhode Island"="Rhode Island","South Carolina"="South Carolina","South Dakota"="South Dakota","Tennessee"="Tennessee","Texas"="Texas","Utah"="Utah","Vermont"="Vermont","Virginia"="Virginia","Washington"="Washington","West Virginia"="West Virginia","Wisconsin"="Wisconsin","Wyoming"="Wyoming"), 
+                   selected = 1),
+       box(plotOutput("singleState")),
+       box(plotOutput("singleStatePP"))
+      )
+    )
+    )
   )
   )
 )
